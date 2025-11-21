96f634988b976342cb830cadb30a8b2fed0a2ddb
kunwar2098 <D.Kunwar@wlv.ac.uk>
2025-11-21 12:28:24 +0545

    Update index.php
    - db setup

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+<?php
+$host = 'localhost';
+$user = '2417131';
+$pass = 'University2025@#$&';
+$dbname = 'db2417131';
 
+$mysqli = new mysqli($host, $user, $pass, $dbname);
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+
+?>
